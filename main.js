8f83dd2fa7bf3ae987ef83a8ef076ba9cd19b6d3
Lucas Burlingham <41302403+lucasburlingham@users.noreply.github.com>
2020-01-31 21:19:55 -0500

    took out references to wrong AppleScript files

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function getLocation() {
-	dofile("./openChrome.scpt");
 	const promptText = "Please enter the full address of the file to download: ";
-	print("<p>Do you want to download the file at " + location + " now? <br>If so, click <a href=&quot" + location + "&quot>here.</a></p>");
+	alert("<p>Do you want to download the file at " + location + " now? <br>If so, click <a href=&quot" + location + "&quot>here.</a></p>");
 	var location = readLine(promptText);
 	if (location.toLowerCase().indexOf('https:') === true || location.toLowerCase().indexOf('http:') === true) { 
 		window.open(location);
 	}
+}
+
+function start() {
+	getLocation();
 }
